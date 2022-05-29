6e0ec8045f11a774175b609c5d3b6b86324a6d12
StanAdim <stanjustine@gmail.com>
2022-05-29 10:21:46 +0300

    app and procfile

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask import Flask,render_template,request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
+
 ###################--- Application  Variable---#####################
 
 
